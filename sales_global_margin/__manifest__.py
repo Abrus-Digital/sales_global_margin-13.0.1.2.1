68014fca4d6d3c3c3bd4f07aafa42929c3076502
Abrusnetworks <kprasanth@abrusnetworks.com>
2020-05-03 09:43:21 +0400

    added the complete module

---
+++ sales_global_margin/__manifest__.py
@@ -0,0 +1,28 @@
+# -*- coding: utf-8 -*-
+# See LICENSE-DOC file for full copyright and licensing details
+{
+    'name': 'Sales Global Margin',
+    'summary': 'Sales Profit Margin',
+    'version': '13.0.1.0.0',
+    'category': 'Sales',
+    'website': 'https://www.abrus.digital/',
+    'description': """Sales Profit Margin.""",
+    'images': ['static/description/banner.jpg'],
+    'author': 'Abrus Digital',
+    'company': 'Abrus Digital',
+    'maintainer': 'Abrus Digital',
+    'support':'info@abrusnetworks.com',
+    'installable': True,
+    'depends': [
+        'base',
+        'sale',
+        'sale_management',
+    ],
+    'data': [
+        'views/sale_order_views.xml',
+    ],
+    'license': 'AGPL-3',
+    'installable': True,
+    'auto_install': False,
+    'application': False,
+}
